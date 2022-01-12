26949ec81c7be6c4144972db715a2d15ec17f3eb
Bartlomiej <bartushod@gmail.com>
2022-01-12 21:19:23 +0100

    commit pliku z obiektem

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
-app.get("/", function (req, res) {
+app.get("/data", function (req, res) {
     res.send("<h1>my app on heroku!-kolejna zmiana w pliku</h1>")
 })
 
